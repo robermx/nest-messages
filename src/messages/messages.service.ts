3ba0580b0cf6e53ed980905f29558434e5362e68
Roberto Arenas <roberto.arenas2@icloud.com>
2023-05-08 13:10:26 -0600

    [Feat] Public and provides included. Mid

--- src/messages/messages.service.ts
+++ src/messages/messages.service.ts
@@ -1,12 +1,9 @@
+import { Injectable } from '@nestjs/common';
 import { MessagesRepository } from './messages.repository';
 
+@Injectable()
 export class MessagesService {
-  messagesRepository: MessagesRepository;
-  constructor() {
-    // Service is creating its own dependencies
-    // DON'T DO THIS ON REAL APPS
-    this.messagesRepository = new MessagesRepository();
-  }
+  constructor(public messagesRepository: MessagesRepository) {}
 
   findOne(id: string) {
     return this.messagesRepository.findOne(id);
